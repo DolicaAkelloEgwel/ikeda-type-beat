ec3db0fcc07730feb31483d9ca2b6fd5d08315fe
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-06-11 18:00:21 +0100

    Got it working

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from app import App
 from app_components import clear_background
 
-class HelloWorld(App):
-  def __init__(self):
-    pass
+class IkedaTypeBeat(App):
+    def __init__(self):
+      pass
 
-  def update(self, delta):
-    pass
+    def update(self, delta):
+      pass
 
-  def draw(self, ctx):
-    clear_background(ctx)
-    ctx.text_align = ctx.CENTER
-    ctx.text_baseline = ctx.MIDDLE
-    ctx.move_to(0, 0).gray(1).text("Hello, world!")
+    def draw(self, ctx):
+        clear_background(ctx)
+        ctx.text_align = ctx.CENTER
+        ctx.text_baseline = ctx.MIDDLE
+        ctx.move_to(0, 0).gray(1).text("Hello, world!")
 
-__app_export__ = HelloWorld
+__app_export__ = IkedaTypeBeat
---
+++ copy.bat
@@ -0,0 +1,2 @@
+mpremote cp app.py :apps/ikeda-type-beat/app.py
+mpremote cp tildagon.toml :apps/ikeda-type-beat/tildagon.toml
