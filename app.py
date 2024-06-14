53929eb84c0960e26c40fc1e460dafacd3251b1b
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-06-14 17:36:27 +0100

    something appears

--- app.py
+++ app.py
@@ -37,21 +37,34 @@ def _sample(population, n_vals):
 class IkedaTypeBeat(App):
     def __init__(self):
         self.button_states = Buttons(self)
-        self.block_pos = []
-        self.speeds = [randint(LOWER_SPEED, UPPER_SPEED) for _ in range(N_ROWS)]
         self.modes = [self._block_stream, self._two_columns]
         self._current = self.modes[0]
         self._index = 0
 
+        # prepare list of points for block stream mode
+        self._block_stream_speeds = [
+            randint(LOWER_SPEED, UPPER_SPEED) for _ in range(N_ROWS)
+        ]
+        self._block_stream_pos = []
         for _ in range(N_ROWS):
             n_block_points = choice(range(LOWER_BLOCK, UPPER_BLOCK, 2))
-            self.block_pos.append(self.get_random_points(n_block_points))
+            self._block_stream_pos.append(self._get_random_points(n_block_points))
 
-    def get_random_points(self, n_block_points):
+        # prepare list of points for columns mode
+        self._column_block_pos = []
+        for _ in range(2):
+            column_points = []
+            block_length = 10
+            n_blocks = SCREEN_SIZE // block_length
+            for _ in range(n_blocks):
+                column_points.append(_sample([i for i in range(10)], 2))
+            self._column_block_pos.append(column_points)
+
+    def _get_random_points(self, n_block_points):
         points = _sample(POSITION_LIST, n_block_points)
         return [[points[i], points[i + 1]] for i in range(0, len(points), 2)]
 
-    def draw_mirror_block(self, ctx, x1, x2, y, h):
+    def _draw_mirror_block(self, ctx, x1, x2, y, h):
         w = x2 - x1
         if x1 > x2:
             ctx.rgb(255, 255, 255).rectangle(x1, y, MAX - x1, h).fill()
@@ -61,13 +74,13 @@ class IkedaTypeBeat(App):
 
     def _block_stream(self, ctx):
         for i in range(N_ROWS):
-            for xs in self.block_pos[i]:
+            for xs in self._block_stream_pos[i]:
                 y = (i * ROW_HEIGHT + GAP_HEIGHT) - MAX
 
-                self.draw_mirror_block(ctx, xs[0], xs[1], y, BLOCK_HEIGHT)
+                self._draw_mirror_block(ctx, xs[0], xs[1], y, BLOCK_HEIGHT)
 
-                xs[0] += self.speeds[i]
-                xs[1] += self.speeds[i]
+                xs[0] += self._block_stream_speeds[i]
+                xs[1] += self._block_stream_speeds[i]
 
                 if xs[0] >= MAX:
                     xs[0] -= SCREEN_SIZE
@@ -75,8 +88,13 @@ class IkedaTypeBeat(App):
                     xs[1] -= SCREEN_SIZE
 
     def _two_columns(self, ctx):
-        ctx.rgb(255, 255, 255).rectangle(-MAX, -MAX, MAX, SCREEN_SIZE).fill()
-        ctx.rgb(0, 0, 0).rectangle(0, 0, MAX, SCREEN_SIZE).fill()
+        for i in range(2):
+            for j in range(len(self._column_block_pos[i])):
+                offset = j * 24 - MAX
+                ys = self._column_block_pos[i][j]
+                ctx.rgb(255, 255, 255).rectangle(
+                    i * MAX - MAX, offset, MAX, ys[1] - ys[0]
+                ).fill()
 
     def update(self, delta):
         if self.button_states.get(BUTTON_TYPES["CANCEL"]):
